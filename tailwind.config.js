c03129d1beaf77b8267077d606cdd1cd9389580e
makachikava <chikavamakanaka@gmail.com>
2023-10-09 15:05:00 -0700

    installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'darkBlue': '#60a5fa',
+      'blue': '#bae6fd',
+      'azul': '#f0f9ff'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
